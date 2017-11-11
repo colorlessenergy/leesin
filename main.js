91e93824fbeb6e89beec0bb26841886d575051b9
colorlessenergy <brianmunozdev@gmail.com>
2017-11-11 07:35:54 -0800

    basic start to animations

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
 const containerMenu = document.querySelector(".container-menu");
 const menuShow = document.querySelector(".menu-show");
 
+const divMove = document.querySelectorAll("main .gallery-wrapper div");
+const websmove = document.querySelectorAll(".other-websites-gallery a div");
+
+
+let once = false;
+let twice = false;
+
 containerMenu.addEventListener("click", function (e) {
   e.currentTarget.classList.toggle("change");
   menuShow.classList.toggle("toggle-nav");
 });
+
+
+window.addEventListener('scroll', function (e) {
+  console.log("called", window.scrollY);
+  if (window.scrollY > 1000 && !once) {
+    divMove.forEach(function (current) {
+      current.classList.toggle("moveimage");
+    });
+    once = true;
+  }
+
+  if (window.scrollY > 3491 && !twice) {
+    websmove.forEach(function (current) {
+      current.classList.toggle("moveimage");
+    });
+    twice = true;
+  }
+})
